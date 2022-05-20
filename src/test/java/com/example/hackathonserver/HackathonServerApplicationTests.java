253d47e2b28b41b664caef5722408645701761ca
mayankarora <mayank.arora@budbee.com>
2022-05-20 15:44:47 +0200

    Initial commit

---
+++ src/test/java/com/example/hackathonserver/HackathonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
